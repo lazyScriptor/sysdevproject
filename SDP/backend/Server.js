268e068044afedcddc86318741241f4e00609b98
lazyScriptor <theekshana.jny@gmail.com>
2024-04-01 21:26:57 +0530

    com 2

--- SDP/backend/Server.js
+++ SDP/backend/Server.js
@@ -5,8 +5,22 @@ const cors = require("cors")
 const app=express()
 app.use(cors())
 
+const db = mysql.createConnection({
+    host:"localhost",
+    user: "root",
+    password:"theekshana123",
+    database:"users"
+})
+
 app.get('/',(req,res)=>{
     return res.json("From backend side");
+})
+app.get('/users',(req,res)=>{
+    const sql="SELECT * FROM users"
+    db.query(sql,(err,data)=>{
+        if(err) return res.json(err);
+        return res.json(data)
+    })
 })
 
 app.listen(8081,()=>{
