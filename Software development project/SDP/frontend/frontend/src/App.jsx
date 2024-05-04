30bc682d30d618640b89127a1b5f78970320f409
lazyScriptor <theekshana.jny@gmail.com>
2024-05-05 03:55:42 +0530

    invoice halfway completed

--- Software development project/SDP/frontend/frontend/src/App.jsx
+++ Software development project/SDP/frontend/frontend/src/App.jsx
@@ -9,7 +9,7 @@ import Inbox from "./components/Pages/Inbox.jsx";
 import Invoice from "./components/Pages/Invoice.jsx";
 import Reports from "./components/Pages/Reports.jsx";
 import Notfoundd from "../additionalcomponents/Notfoundd.jsx";
-import './index.css'
+// import './index.css'
 
 import { useState, createContext } from "react";
 
@@ -40,28 +40,28 @@ function App() {
         // 900: "#006064",
 
         //Blue shades
-        50: "#e3f2fd",
-        100: "#bbdefb",
-        200: "#90caf9",
-        300: "#64b5f6",
-        400: "#42a5f5",
-        500: "#2196f3",
-        600: "#1e88e5",
-        700: "#1976d2",
-        800: "#1565c0",
-        900: "#0d47a1",
+        // 50: "#e3f2fd",
+        // 100: "#bbdefb",
+        // 200: "#90caf9",
+        // 300: "#64b5f6",
+        // 400: "#42a5f5",
+        // 500: "#2196f3",
+        // 600: "#1e88e5",
+        // 700: "#1976d2",
+        // 800: "#1565c0",
+        // 900: "#0d47a1",
 
         // purpleShades
-        // 50: "#f3e5f5",
-        // 100: "#e1bee7",
-        // 200: "#ce93d8",
-        // 300: "#ba68c8",
-        // 400: "#ab47bc",
-        // 500: "#9c27b0",
-        // 600: "#8e24aa",
-        // 700: "#7b1fa2",
-        // 800: "#6a1b9a",
-        // 900: "#4a148c",
+        50: "#f3e5f5",
+        100: "#e1bee7",
+        200: "#ce93d8",
+        300: "#ba68c8",
+        400: "#ab47bc",
+        500: "#9c27b0",
+        600: "#8e24aa",
+        700: "#7b1fa2",
+        800: "#6a1b9a",
+        900: "#4a148c",
 
         //orange shades
         // 50: "#FFF3E0",
--- Software development project/SDP/frontend/frontend/src/components/Pages/Invoice.jsx
+++ Software development project/SDP/frontend/frontend/src/components/Pages/Invoice.jsx
@@ -2,7 +2,18 @@ import React from "react";
 import "../Stylings/rootstyles.css";
 import NewCustomerForm from "./NewCustomerForm.jsx";
 import BackgroundStyleNew from "../SubComponents/BackgroundStyleNew.jsx";
-import { Box, Button, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  FormLabel,
+  Paper,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch, faUpload } from "@fortawesome/free-solid-svg-icons";
+import Checkbox from "@mui/material/Checkbox";
+import CustomerTable from "../SubComponents/CustomerTable.jsx";
 
 function Invoice() {
   return (
@@ -15,7 +26,6 @@ function Invoice() {
           alignItems: "center",
           Width: "100%",
           minHeight: "100vh",
-          border: "solid 8px green",
         }}
       >
         {/* Row1 */}
@@ -24,7 +34,6 @@ function Invoice() {
           sx={{
             display: "flex",
             width: "100%",
-            border: "solid 3px blue",
             height: "30%",
           }}
         >
@@ -61,14 +70,14 @@ function Invoice() {
           <Box
             sx={{
               display: "flex",
-              justifyContent: "center",
+              justifyContent:"space-evenly",
               alignItems: "center",
               width: "25%",
               gap: 2,
             }}
           >
-            {" "}
-            <Button>Create new</Button>
+            
+            <Button variant="contained">Create new</Button>
             {/* Invoice text box */}
             <Box>
               <h5>Invoice ID: 001</h5>
@@ -83,7 +92,6 @@ function Invoice() {
           sx={{
             display: "flex",
             width: "100%",
-            border: "solid 3px blue",
             minheight: "30%",
           }}
         >
@@ -99,6 +107,7 @@ function Invoice() {
             <h1>hey</h1>
           </Box>
           {/*Row2 middle box */}
+
           <Box
             sx={{
               display: "flex",
@@ -108,47 +117,243 @@ function Invoice() {
               width: "60%",
             }}
           >
-            {/* 2nd row middle box left */}
-            <Box
-              sx={{ display: "flex", width: "35%", justifyContent: "center" }}
+            <Paper
+              elevation={3}
+              sx={{
+                width: "95%",
+                display: "flex",
+                justifyContent: "center",
+                p: 3,
+                borderRadius: 3,
+              }}
             >
-              <h1>hey</h1>
-            </Box>
-            {/* 2nd row middle box right */}
+              {/* 2nd row middle box left */}
+              <Box
+                sx={{
+                  display: "flex",
+                  width: "35%",
+                  justifyContent: "start",
+                  flexDirection: "column",
+                  gap: 7.4,
+                  pt: 12.8,
+                }}
+              >
+                <FormLabel>Customer Name</FormLabel>
+                <FormLabel>Customer Address</FormLabel>
+                <FormLabel>Upload NIC</FormLabel>
+                <FormLabel>Customer Phone number</FormLabel>
+              </Box>
+              {/* 2nd row middle box right */}
 
-            <Box
-            //  2nd row middle box right-(Searchid,search box) AND (clear button)
-              sx={{ display: "flex", width: "65%", justifyContent: "start" ,gap:10}}
-            >
-               {/* 2nd row middle box right - searchbar and search button only */}
-              <Box sx={{display:"flex", gap:3}}>
-                <TextField
-                  sx={{ width: "350px" }}
-                  id="outlined-basic"
-                  label="Search with id"
-                  variant="outlined"
-                />
-                <Button>
-                  Search
-                </Button>
+              <Box
+                //  2nd row middle box right-(Searchid,search box) AND (clear button)
+                sx={{
+                  display: "flex",
+                  width: "65%",
+                  justifyContent: "start",
+                  flexDirection: "column",
+                  gap: 3,
+                }}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    width: "100%",
+                    gap: 2,
+                  }}
+                >
+                  {/* 2nd row middle box right - searchbar and search button only */}
+                 
+                    <TextField
+                      sx={{ width: "350px" }}
+                      id="outlined-basic"
+                      label="Search with id"
+                      variant="outlined"
+                    />
+                    <Button sx={{borderRadius:"50%"}}><FontAwesomeIcon icon={faSearch}/></Button>
+                  
+                  <Button
+                    sx={{ color: (theme) => theme.palette.primary.error }}
+                  >
+                    Clear
+                  </Button>
                 </Box>
-                <Button>
-                  Clear
-                </Button>
-              
-            </Box>
+
+                <Box>
+                  <TextField
+                    fullWidth
+                    sx={{}}
+                    id="outlined-basic"
+                    label="Customer Name"
+                    variant="outlined"
+                  />
+                </Box>
+                <Box>
+                  <TextField
+                    fullWidth
+                    sx={{}}
+                    id="outlined-basic"
+                    label="Address"
+                    variant="outlined"
+                  />
+                </Box>
+                <Box sx={{ display: "flex", gap: 4 }}>
+                  <TextField
+                    sx={{}}
+                    id="outlined-basic"
+                    label={<FontAwesomeIcon icon={faUpload} />}
+                    variant="outlined"
+                  />
+                  <Checkbox defaultChecked /><FormLabel sx={{pt:2}}>Physical</FormLabel>
+                  <Checkbox /><FormLabel sx={{pt:2}}>Digital</FormLabel>
+                </Box>
+                <Box>
+                  <TextField
+                    fullWidth
+                    sx={{}}
+                    id="outlined-basic"
+                    label="Phone number"
+                    variant="outlined"
+                  />
+                </Box>
+                <Box sx={{ display: "flex", justifyContent: "center" }}>
+                  <Button variant="contained">
+                    Proceed to <br />
+                    payment
+                  </Button>
+                </Box>
+              </Box>
+            </Paper>
           </Box>
+
           {/*Row2 rightmost box */}
           <Box
             sx={{
               display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
+              flexDirection: "column",
+              gap: 3,
               width: "25%",
             }}
           >
-            {" "}
-            <h1>hey</h1>
+            <Paper
+              elevation={3}
+              sx={{
+                width: "95%",
+                display: "flex",
+                justifyContent: "center",
+                p: 1,
+                borderRadius: 3,
+                height: "70%",
+              }}
+            >
+              <Box sx={{ width: "90%"}}>
+                <Typography variant="h5" gutterBottom>
+                  Add / Remove / Handover
+                </Typography>
+                <Typography
+                  sx={{ textAlign: "center" }}
+                  variant="h5"
+                  gutterBottom
+                >
+                  Equipment
+                </Typography>
+                <Box gap={2} sx={{ display:"flex",flexDirection:"column",height:"auto"}}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      width: "100%",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        display: "flex",
+                        width: "40%",
+                        alignItems: "center",
+                      }}
+                    >
+                      <FormLabel htmlFor="my-input">Equipment Id</FormLabel>
+                    </Box>
+                    <Box sx={{ width: "60%" }}>
+                      <TextField
+                        sx={{ width: "100%" }}
+                        id="outlined-basic"
+                        label="Search machine id"
+                        variant="outlined"
+                      />
+                    </Box>
+                  </Box>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      flexDirection: "row",
+                      width: "100%",
+                    }}
+                  >
+                    <Box
+                      sx={{
+                        width: "40%",
+                        display: "flex",
+                        alignItems: "center",
+                      }}
+                    >
+                      <FormLabel htmlFor="my-input">Equipment Name</FormLabel>
+                    </Box>
+                    <Box sx={{ width: "60%" }}>
+                      <TextField
+                        sx={{ width: "100%" }}
+                        id="outlined-basic"
+                        variant="outlined"
+                      />
+                    </Box>
+                  </Box>
+                </Box>
+
+
+
+                <Box sx={{display:"flex",justifyContent:"space-between",p:3}}>
+                  <Button>
+                    Add
+                  </Button>
+                  <Button>
+                    Remove
+                  </Button>
+                  <Button>
+                    Handover
+                  </Button>
+                </Box>
+              </Box>
+            </Paper>
+            <Paper
+              elevation={3}
+              sx={{
+                width: "95%",
+                display: "flex",
+                justifyContent: "center",
+                p: 3,
+                borderRadius: 3,
+                height: "30%",
+              }}
+            >
+              <Box sx={{width:"100%"}}>
+                <Typography sx={{textAlign:"center"}}variant="h5" gutterBottom>
+                  Handover Id Card
+                </Typography>
+                <Box sx={{width:"100%",display:"flex",justifyContent:"space-evenly"}}>
+                  <Button>
+                    Add
+                  </Button>
+                  <Button>
+                    Remove
+                  </Button>
+                  <Button>
+                    Handover
+                  </Button>
+                </Box>
+              </Box>
+              
+            </Paper>
           </Box>
         </Box>
 
@@ -158,7 +363,6 @@ function Invoice() {
           sx={{
             display: "flex",
             width: "100%",
-            border: "solid 3px blue",
             minheight: "30%",
           }}
         >
@@ -182,8 +386,7 @@ function Invoice() {
               width: "60%",
             }}
           >
-            {" "}
-            <h1>hey</h1>
+           3rd row middle
           </Box>
           {/*Row3 rightmost box */}
           <Box
